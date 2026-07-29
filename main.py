244f63b8a781241bc09d7e9b378f19b877ac0394
layla <laylacoletti@gmail.com>
2026-07-29 16:24:41 -0400

    Started to build clock window

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello, Desk Companion!")
+import tkinter as tk 
+
+
+from datetime import datetime
+
+window = tk.Tk()
+
+window.title("Desk Companion")
+
+
+current_time = datetime.now().strtime("%I:%M %p")
+
+
+clock_lable = tk.Lable(window, text=current_time)
+
+clock_lable.pack(padx=40, pady=40)
+
+
+window.mainloop()
