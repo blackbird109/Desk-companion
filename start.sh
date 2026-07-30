118c8f813c63b70241eed54609a0f30cdfd61634
layla <laylacoletti@gmail.com>
2026-07-30 15:10:42 -0400

    started to work on automation

---
+++ start.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# Give the desktop a few seconds to finish loading.
+sleep 5
+
+# Move into the Desk Companion project folder.
+cd "/Users/layla/Projects/desk-companion"
+
+# Start the clock.
+python3 main.py
