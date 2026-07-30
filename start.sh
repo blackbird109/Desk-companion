8d61128c64fc23701a5b40d9fca9148bd1ed5401
blackbird109 <laylacoletti@gmail.com>
2026-07-30 19:20:45 -0400

    Fix autostart script

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
-#/bin/bash
+#!/bin/bash
 
-# Give the desktop a few seconds to finish loading.
+# Wait for the desktop to finish loading
 sleep 5
 
-# Move into the Desk Companion project folder.
-cd "/Users/layla/Projects/desk-companion"
+# Tell GUI programs which display to use
+export DISPLAY=:0
 
-# Start the clock.
+# Go to your project
+cd "/home/laylarules109/code/Desk-companion"
+
+# Launch the program
 python3 main.py
