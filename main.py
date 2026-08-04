4473ce0f15b6d41dea33ff89717743a7caa9040d
layla <laylacoletti@gmail.com>
2026-08-04 12:19:04 -0400

    created a greeting and changed bc and fg color

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk 
 from datetime import datetime
 
+USER_NAME = "Layla Coletti"
+
 
 window = tk.Tk()
 window.title("TIK TOCK DOC")
@@ -10,12 +12,26 @@ window.bind("<Escape>", lambda event: window.destroy())
 
 
 def update_clock():
+    current_hour = datetime.now().hour
+    if current_hour < 12:
+        greeting = f"Good morning, {USER_NAME}!"
+    elif current_hour < 18:
+        greeting = f"Good afternoon, {USER_NAME}!" 
+    else:
+        greeting = f"Good evening, {USER_NAME}!"
     current_time = datetime.now().strftime("%I:%M:%S %p")
     clock_label.config(text=current_time)
     window.after(1000, update_clock)
+    greeting_label.config(text=greeting)
+    greeting_label.pack(pady=(40,10))
 
-
-
+greeting_label = tk.Label(
+        window,
+        text="",
+        font=("Ariel", 50),
+        fg="Dark Green",
+        bg="Cornsilk"
+    )
 clock_label = tk.Label(
     window, 
     text="",
